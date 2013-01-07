4695cfd16335e8b213cee6c40c47587f69f3f9b5
Simon Coffey <simon@tribesports.com>
2013-01-07 19:25:55 +0000

    Disable XML params parsing

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,11 +1,10 @@
-# Be sure to restart your server when you modify this file.
-
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
 Mime::Type.register "application/vnd.derp.api.v1+json", :api_v1
 
+# Remove XML from default parsers for security reasons
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
 # Tell ActionDispatch to parse API_V1 post/put bodies as JSON
-Derp::Application.config.middleware.delete "ActionDispatch::ParamsParser"
-Derp::Application.config.middleware.use ActionDispatch::ParamsParser, { Mime::API_V1 => :json }
+ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::API_V1] = :json
